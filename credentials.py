85855297592f65e7c9d188dd5b425ec15d72ecb1
Nicholas Siconolfi <nicholas.siconolfi1@marist.edu>
2018-09-25 17:33:45 -0400

    Beginning of credential program.

---
+++ credentials.py
@@ -0,0 +1,24 @@
+# CMPT 120 Intro to Programming
+# Lab #5 – Working with Strings and Functions
+# Author: Your Name Here
+# Created: YYYY-MM-DD
+def main():
+# get user's first and last names
+    first = input("Enter your first name: ")    
+    last = input("Enter your last name: ")
+# TODO modify this to generate a Marist-style username
+    uname = first + last
+# ask user to create a new password
+    passwd = input("Create a new password: ")
+# TODO modify this to ensure the password has at least 8 characters
+    while False:
+        print("Fool of a Took! That password is feeble!")
+        passwd = input("Create a new password: ")
+    print("The force is strong in this one…")
+    print("Account configured. Your new email address is",
+    uname + "@marist.edu")
+
+
+
+main()
+
