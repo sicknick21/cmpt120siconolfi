2547b3971c25c45f8cfd89079b0d0738974b065a
Your Name <your-email-address>
2018-09-27 17:57:21 -0400

    Added the full part of the credential program with all modifications.

--- credentials.py
+++ credentials.py
@@ -9,27 +9,43 @@ def firstlastname():
     
     first = input("Enter your first name: ")    
     last = input("Enter your last name: ")
+    first = first.lower()
+    last = last.lower()
 
 def username():
+    global uname
     
-    uname = first + last
-    print("Hi" , first+"."+last)
+    uname = first +"."+ last
+   
 
 def password():
-
+    global passwd
     passwd = input("Create a new password: ")
 
-    while False:
-        print("Fool of a Took! That password is feeble!")
-        passwd = input("Create a new password: ")
-    print("The force is strong in this one…")
 
-    
-    
 
+def strength():
+    if passwd.isupper():
+        print("Password needs a lowercase letter")
+    if passwd.islower():
+        print("Password needs a capital letter")
+              
+    
+    if len(passwd) <8:
+           print("Fool of a Took! That password is feeble!")
+    if len(passwd) >8:
+        print("The force is strong in this one…")
+           
+def email():
+    
+    print("Account configured. Your new email address is", uname + "@marist.edu")
+
+      
 def main():
     firstlastname()
     username()
     password()
+    strength()
+    email()
 
 main()
