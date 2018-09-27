fb15f0bdfa93ef71b11511bffd0483b51969f949
Your Name <your-email-address>
2018-09-27 17:14:38 -0400

    Added the first part of the credential file.

--- credentials.py
+++ credentials.py
@@ -2,23 +2,34 @@
 # Lab #5 – Working with Strings and Functions
 # Author: Your Name Here
 # Created: YYYY-MM-DD
-def main():
-# get user's first and last names
+
+
+def firstlastname():
+    global uname, first, last
+    
     first = input("Enter your first name: ")    
     last = input("Enter your last name: ")
-# TODO modify this to generate a Marist-style username
+
+def username():
+    
     uname = first + last
-# ask user to create a new password
+    print("Hi" , first+"."+last)
+
+def password():
+
     passwd = input("Create a new password: ")
-# TODO modify this to ensure the password has at least 8 characters
+
     while False:
         print("Fool of a Took! That password is feeble!")
         passwd = input("Create a new password: ")
     print("The force is strong in this one…")
-    print("Account configured. Your new email address is",
-    uname + "@marist.edu")
 
+    
+    
 
+def main():
+    firstlastname()
+    username()
+    password()
 
-main()
-
+main()
